6fe7efff79dac8f55e898a5cf9166954e82bf799
hydrae-soft <torres.lf21@gmail.com>
2023-07-11 15:07:16 -0300

    Implemented: TailwindCSS.

--- src/main.ts
+++ src/main.ts
@@ -1,114 +1,68 @@
-import './scss/style.scss'
-import {NaNError, DayOutOfMonthLimitsError, InvalidMonthError, InvalidYearError} from './modules/errors'
-import { monthIsInFuture, exceedsMonthDays } from './modules/date-utils'
+import { NaNError, InvalidDayError, InvalidMonthError, InvalidYearError } from './modules/errors.js'
+import { monthIsValid, dayIsValid } from './modules/date-validation.ts'
+import './style.css'
+// Create constant for get today's date.
 const TODAY = new Date(Date.now());
-(document.getElementById('year') as HTMLInputElement).oninput = (event: Event) => {
-  const target = event.target as HTMLInputElement
-  const value = target.value
-  const year = TODAY.getFullYear()
-  if (parseInt(value) > year)
-    target.value = year.toString()
-}
-(document.getElementById('month') as HTMLInputElement).oninput = (event: Event) => {
-  const target = event.target as HTMLInputElement
-  const value = target.value
-  if (value.length > 2)
-    target.value = value.substring(0, 2)
-}
-(document.getElementById('day') as HTMLInputElement).oninput = (event: Event) => {
-  const target = event.target as HTMLInputElement
-  const value = target.value
-  if (value.length > 2)
-    target.value = value.substring(0, 2)
-}
-interface ValidInputsFormat {
-  [key: string]: number
-}
-function getInputs() {
-  const validInputs: ValidInputsFormat = {
+// Add submit listener to birthday-form.
+const form = document.getElementById('birthday-form')
+if (form instanceof HTMLFormElement)
+  form.onsubmit = event => {
+    event.preventDefault()
+    // Remove invalidations.
+    document.querySelectorAll('span.invalid').forEach(column => {
+      column.classList.remove('invalid')
+      const p = column.lastElementChild
+      if (p instanceof HTMLParagraphElement)
+        p.innerHTML = ''
+    })
+    try {
+      const { day, month, year } = parseInputs()
+      validateInputs(day, month, year)
+      const result = calculate(new Date(year, month, day))
+      printResult(result)
+    } catch (error) {
+      const column = document.querySelector(`#${error.origin}-input-column`)
+      if (column instanceof HTMLSpanElement) {
+        column.classList.add('invalid');
+        (column.lastElementChild as HTMLParagraphElement).innerHTML = error.message
+      }
+    }
+  }
+function parseInputs () {
+  const numericInputs = {
     day: 0,
     month: 0,
     year: 0
   }
-  const fields = document.querySelectorAll('input[type="number"]') as NodeListOf<HTMLInputElement>
-  fields.forEach(field => {
-    const value = parseInt(field.value)
-    if (isNaN(value))
-      throw new NaNError(field.id)
-    else validInputs[field.id] = value
-  })
-  return validInputs
+  document.querySelectorAll('input[type="number"]')
+    .forEach(field => {
+      const value = parseInt(field?.value)
+      if (isNaN(value)) throw new NaNError(field.id)
+      else numericInputs[field.id] = value
+    })
+  numericInputs.month--
+  return numericInputs
 }
-function validateInputs(inputs: ValidInputsFormat) {
-  const keys = Object.keys(inputs)
-  keys.forEach(key => {
-    switch(key) {
-      case 'day':
-        if (inputs['day'] < 1 || exceedsMonthDays(inputs['day'], inputs['month'], inputs['year']))
-          throw new DayOutOfMonthLimitsError()
-          break
-      case 'month':
-        if ((inputs['month'] < 0) || (monthIsInFuture(inputs['month'], inputs['year'])))
-          throw new InvalidMonthError()
-          break
-      case 'year':
-        if ((inputs['year'] < 1900) || (inputs['year'] > TODAY.getFullYear()))
-          throw new InvalidYearError()
-    }
-  });
+function validateInputs (day: number, month: number, year: number) {
+  if ((year < 1900) || (year > TODAY.getFullYear())) throw new InvalidYearError()
+  if (!monthIsValid(month, year)) throw new InvalidMonthError()
+  if (!dayIsValid(day, month, year)) throw new InvalidDayError()
 }
-function calculate(birthday: Date) {
-  // Get birthday and today's date difference in milliseconds per day
+function calculate (birthday: Date) {
+  // Get difference between today and birthday in milliseconds per day.
   const daysInMills = (((new Date(Date.UTC(TODAY.getFullYear(), TODAY.getMonth(), TODAY.getDate()) - birthday.getTime()).getTime() / 1000) / 60) / 60) / 24
   const years = Math.floor(daysInMills / 365)
-  // Get remaining months after the birthday
+  console.log('days ', TODAY.getFullYear(), TODAY.getMonth(), TODAY.getDate())
+  // Get remaining months after the current year's birthday.
   const months = Math.floor((daysInMills / (365 / 12)) - 12 * years)
-  // Get remaining days after the birthday
+  // Get remaining days after the current year's birthday.
   const days = Math.floor(daysInMills - 365 * years - (365 / 12) * months)
-  printResult(days, months, years)
+  return {days, months, years}
 }
-function printResult(days: number, months: number, years: number) {
-  const [span_years, span_months, span_days] = (document.querySelectorAll('span.number') as NodeListOf<HTMLSpanElement>)
-  span_days.innerHTML = `${days}`
-  span_months.innerHTML = `${months}`
-  span_years.innerHTML = `${years}`
+function printResult (date: {days: number, months: number, years: number}) {
+  const {days, months, years} = date
+  const [spanYears, spanMonths, spanDays] = document.querySelectorAll('span.number')
+  spanDays.innerHTML = `${days}`
+  spanMonths.innerHTML = `${months}`
+  spanYears.innerHTML = `${years}`
 }
-(document.querySelector('.birthday-form') as HTMLFormElement).onsubmit = ((event: SubmitEvent) => {
-  event.preventDefault()
-  try {
-    const inputs = getInputs()
-    validateInputs(inputs)
-    calculate(new Date(inputs['year'], inputs['month'] - 1, inputs['day']))
-    const columns = document.querySelectorAll('span.form-column') as NodeListOf<HTMLSpanElement>
-    columns.forEach(column => {
-      column.classList.remove('invalid')
-    });
-  } catch(error: any) {
-    switch (error.name) {
-      case 'NaNError': {
-        const column = document.querySelector(`#${error.origin}-input-column`) as HTMLSpanElement
-        column.classList.add('invalid');
-        (column.lastElementChild as HTMLParagraphElement).innerHTML = 'You must enter a number.'
-        break
-      }
-      case 'DayOutOfMonthLimitsError': {
-        const column = document.querySelector('#day-input-column') as HTMLSpanElement
-        column.classList.add('invalid');
-        (column.lastElementChild as HTMLParagraphElement).innerHTML = 'Input .'
-        break
-      }
-      case 'InvalidMonthError': {
-        const column = document.querySelector('#month-input-column') as HTMLSpanElement
-        column.classList.add('invalid');
-        (column.lastElementChild as HTMLParagraphElement).innerHTML = 'Month is not valid.'
-        break
-      }
-      case 'InvalidYearError': {
-        const column = document.querySelector('#year-input-column') as HTMLSpanElement
-        column.classList.add('invalid');
-        (column.lastElementChild as HTMLParagraphElement).innerHTML = 'Year is not valid.'
-        break
-      }
-    }
-  }
-})
--- src/modules/errors.ts
+++ src/modules/errors.ts
@@ -1,25 +1,28 @@
 export class NaNError extends Error {
   origin
-  constructor(origin: string) {
-    super('The value must be enter a number.')
+  constructor (origin: string) {
+    super('The value must be a number.')
     this.name = 'NaNError'
     this.origin = origin
   }
 }
-export class DayOutOfMonthLimitsError extends Error {
-  constructor() {
-    super('The entered day is out of the month range.')
-    this.name = 'DayOutOfMonthLimitsError'
+export class InvalidDayError extends Error {
+  origin = 'day'
+  constructor () {
+    super('The entered day is out of range.')
+    this.name = 'InvalidDayError'
   }
 }
 export class InvalidMonthError extends Error {
-  constructor() {
+  origin = 'month'
+  constructor () {
     super('The entered month does not exist.')
     this.name = 'InvalidMonthError'
   }
 }
 export class InvalidYearError extends Error {
-  constructor() {
+  origin = 'year'
+  constructor () {
     super('The entered year does not exist.')
     this.name = 'InvalidYearError'
   }
