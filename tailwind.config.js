18332bc7bdc1cea91905166e9ea2a7681570c92c
lftdev <torres.lf21@gmail.com>
2023-07-27 15:45:55 -0300

    Fix general styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     './src/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {}
+    extend: {},
+    screens: {
+      tablet: '640px',
+      // => @media (min-width: 640px) { ... }
+
+      laptop: '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      desktop: '1280px'
+      // => @media (min-width: 1280px) { ... }
+    }
   },
   plugins: []
 }
